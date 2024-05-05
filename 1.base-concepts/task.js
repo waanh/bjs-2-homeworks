14239300a8af46ff3cf17b1e325f1d4ad3b3babf
waanh <anhimovalera@gmail.com>
2024-05-05 13:53:03 +0300

    1 task

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,17 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let discriminant = (b ** 2) - (4 * a * c);
+  if (discriminant < 0) {
+    arr = [];
+  } else if (discriminant === 0) {
+    let root = -b/(2*a);
+    arr = [root];
+  } else (discriminant > 0) {
+    let root_1 = (-b + Math.sqrt(d) )/(2*a);
+    let root_2 = (-b - Math.sqrt(d) )/(2*a);
+    arr = [root_1, root_2];
+  }
   return arr;
 }
 
