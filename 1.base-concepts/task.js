f26d755bc73c90f5ccb902747849922837bd6619
waanh <anhimovalera@gmail.com>
2024-05-10 16:24:04 +0300

    tasks

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -2,19 +2,26 @@
 function solveEquation(a, b, c) {
   let arr = [];
   let discriminant = (b ** 2) - (4 * a * c);
-  if (discriminant < 0) {
-    arr = [];
-  } else if (discriminant === 0) {
+  if (discriminant === 0) {
     let root = -b/(2*a);
-    arr = [root];
-  } else (discriminant > 0) {
-    let root_1 = (-b + Math.sqrt(d) )/(2*a);
-    let root_2 = (-b - Math.sqrt(d) )/(2*a);
-    arr = [root_1, root_2];
+    arr.push(root);
+  } else if (discriminant > 0) {
+    let root_1 = (-b + Math.sqrt(discriminant) )/(2*a);
+    let root_2 = (-b - Math.sqrt(discriminant) )/(2*a);
+    arr.push(root_1, root_2);
   }
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  percent = percent / 100;
+    let bodyCredit = amount - contribution;
+    let monthlyPercent = percent / 12;
+    let countPayments = countMonths;
+    let monthlyPayment = bodyCredit * (monthlyPercent + monthlyPercent / (Math.pow(1 + monthlyPercent, countPayments) - 1));
+    let totalAmount = monthlyPayment * countPayments;
+
+    totalAmount = parseFloat(totalAmount.toFixed(2));
+
+    return totalAmount;
 }
