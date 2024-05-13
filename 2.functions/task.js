ca7d5f03bb6d856c6d8967d5da0a5ee1130c7b34
waanh <anhimovalera@gmail.com>
2024-05-13 15:44:11 +0300

    task

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,4 +1,20 @@
 function getArrayParams(...arr) {
+  let min = arr[0];
+  let max = arr[0];
+  let sum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+      if (arr[i] < min) {
+          min = arr[i];
+      }
+      if (arr[i] > max) {
+          max = arr[i];
+      }
+      sum += arr[i];
+  }
+
+  let avg = sum / arr.length;
+  avg = parseFloat(avg.toFixed(2));
 
   return { min: min, max: max, avg: avg };
 }
